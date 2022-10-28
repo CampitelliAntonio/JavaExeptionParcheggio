4e79d512cb4740b2e77c542c69c5c1277a579e59
Campitelli Antonio <campitelli.antonio@osdbsesto>
2022-10-28 14:32:18 +0200

    programma finito

--- parcheggio/parcheggioAuto/src/main/Main.java
+++ parcheggio/parcheggioAuto/src/main/Main.java
@@ -2,8 +2,58 @@ package main;
 
 import java.util.Scanner;
 
+//        long differenza = macchine[0].getUscita().getTime() - macchine[0].getEntrata().getTime();
+//        System.out.println("La differenza di Timestamp tra l'entrata e l'uscita è " + differenza);
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        automobile[] macchine = new automobile[10];
+        for (int i = 0; i < macchine.length; i++) {
+            macchine[i] = new automobile();
+        }
+        String azione;
+        long media = 0;
+        int autoTot = 0;
+        while (true) {
+            System.out.println("Vuoi inserire un'altra macchina, farne uscire una o la media degli orari di permanenza (inserire, uscire, media)");
+            azione = in.next();
+            switch (azione) {
+                case "inserire":
+                    for (int i = 0; i < macchine.length; i++) {
+                        if (macchine[i].getTarga().equals("")) {
+                            System.out.println("Scrivi la targa della macchina");
+                            macchine[i] = new automobile(in.next(), System.currentTimeMillis());
+                            i = macchine.length;
+                        } else if (i == macchine.length - 1) {
+                            System.out.println("Il parcheggio è pieno");
+                        }
+                    }
+                    break;
+                case "uscire":
+                    long differenza = 0;
+                    System.out.println("Inserisci la targa della macchina che vuoi far uscire");
+                    String targa = in.next();
+                    for (int i = 0; i < macchine.length; i++) {
+                        try {
+                            if (macchine[i].getTarga().equals(targa)) {
+                                macchine[i].setUscita(System.currentTimeMillis());
+                                i = macchine.length;
+                                differenza = (macchine[0].getUscita().getTime() - macchine[0].getEntrata().getTime()) / 1000;
+                                System.out.println("La differenza di Timestamp tra l'entrata e l'uscita è " + differenza);
+                            } else if (i == macchine.length - 1) {
+                                System.out.println("Non esiste una macchina con questa targa: " + targa);
+                            }
+                        } catch (Exception e) {
+                            System.out.println("Non esiste una macchina con questa targa: " + targa);
+                            i = macchine.length;
+                        }
+                    }
+                    media += differenza;
+                    autoTot++;
+                    break;
+                case "media":
+                    System.out.println("La media è " + media / autoTot);
+            }
+        }
     }
 }
--- parcheggio/parcheggioAuto/src/main/automobile.java
+++ parcheggio/parcheggioAuto/src/main/automobile.java
@@ -1,4 +1,37 @@
 package main;
+import java.sql.Timestamp;
 
 public class automobile {
+    private String targa;
+    private Timestamp entrata;
+    private Timestamp uscita;
+
+    public automobile() {
+        this.targa = "";
+        this.entrata = null;
+    }
+
+    public automobile(String targa, long entrata) {
+        this.targa = targa;
+        this.entrata = new Timestamp(entrata);
+        System.out.println("Il Timestamp di entrata della macchina " + this.targa + ": " + this.entrata.getTime());
+    }
+
+    public void setUscita(long uscita) {
+        this.uscita = new Timestamp(uscita);
+        System.out.println("Il Timestamp di uscita della macchina " + this.targa + ": " + this.uscita.getTime());
+        this.targa = "";
+    }
+
+     public String getTarga(){
+        return targa;
+     }
+
+    public Timestamp getEntrata() {
+        return entrata;
+    }
+
+    public Timestamp getUscita() {
+        return uscita;
+    }
 }
