80ebd54be043f2b469723f32008a1e0448a4ee3a
Campitelli Antonio <campitelli.antonio@osdbsesto>
2022-11-04 14:03:07 +0100

    aggiunta loop di inserzione e rimozione macchina

--- parcheggio/parcheggioAuto/src/main/Main.java
+++ parcheggio/parcheggioAuto/src/main/Main.java
@@ -4,29 +4,32 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         Scanner in = new Scanner(System.in);
-
         Parcheggio par = new Parcheggio();
-
-        try {
-            System.out.println("inserire targa");
-            par.aggiungiMacchina(new automobile(in.next(),
-                    System.currentTimeMillis()), 0);
-
-            System.out.println("inserire targa");
-            par.aggiungiMacchina(new automobile(in.next(),
-                    System.currentTimeMillis()), 1);
-
-            System.out.println("inserire targa");
-            par.aggiungiMacchina(new automobile(in.next(),
-                    System.currentTimeMillis()), 2);
-        } catch (Exception e) {
-            System.out.println("Errore " + e.getMessage());
+        int posizione = 0;
+        String azione;
+        boolean uscire = false;
+        while (!uscire) {
+            System.out.println("\nChe azione vuoi fare? (aggiungere, uscire)");
+            azione = in.next().toLowerCase();
+            switch (azione){
+                case "aggiungere":
+                    try {
+                        System.out.println("Inserisci la targa");
+                        par.aggiungiMacchina(new automobile(in.next(), System.currentTimeMillis()), posizione);
+                        posizione ++;
+                    } catch (Exception e) {
+                        System.out.println("Errore " + e.getMessage());
+                    }
+                    break;
+                case "uscire":
+                    System.out.println("Scrivi la targa della macchina da far uscire");
+                    long secondi = par.rimuoviMacchina(in.next());
+                    System.out.println("Sono passati " + secondi + " dall'entrata nel parcheggio di questa macchina");
+                    break;
+                default:
+                    uscire = true;
+            }
         }
-
-        System.out.println("che macchina vuoi far uscire");
-        long secondi = par.rimuoviMacchina(in.next());
-        System.out.println("sono passati " + secondi);
     }
 }
